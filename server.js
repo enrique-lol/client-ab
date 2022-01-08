0c869fdf8ac72127bb7f8a378a85429b6938d40d
enrique-lol <enrique.memije.pro@gmail.com>
2022-01-07 18:12:49 -0600

    New Collection

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 // require route files
 const itemRoutes = require('./app/routes/item_routes')
 const userRoutes = require('./app/routes/user_routes')
+const collectionRoutes = require('./app/routes/collection_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -58,6 +59,7 @@ app.use(requestLogger)
 // register route files
 app.use(itemRoutes)
 app.use(userRoutes)
+app.use(collectionRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
